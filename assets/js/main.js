03fb6f2ff6e07ebad98638152fbc28786377c6e1
Kaique Feitosa <kaique.cmv@gmail.com>
2024-10-26 10:16:26 -0300

    Adiciona projeto finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const audio = document.getElementById("music");
+
+function switchTheme() {
+  const body = document.body;
+
+  body.classList.toggle("dark-theme");
+  body.classList.toggle("light-theme");
+
+  if (body.classList.contains("dark-theme")) {
+    audio.src = "./assets/musics/inverted-world.mpeg";
+  } else {
+    audio.src = "./assets/musics/normal-world.mpeg";
+  }
+
+  audio.load();
+}
